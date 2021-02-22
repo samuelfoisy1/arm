75f28742afef597cbb25fdfdeface186d4310bf4
Steev Klimaszewski <steev@kali.org>
2021-02-22 10:57:39 -0600

    rpi*: Use kali-pi-next for 4.19.127 kernel
    We want the newer kernel, so for this release, bump to using -next to
    get it.

--- rpi.sh
+++ rpi.sh
@@ -259,7 +259,7 @@ wget -q ${githubraw}/Re4son/RPi-Tweaks/master/kalipi-tft-config/kalipi-tft-confi
 chmod 755 /usr/bin/kalipi-tft-config
 
 # Install the kernel packages
-echo "deb http://http.re4son-kernel.com/re4son kali-pi main" > /etc/apt/sources.list.d/re4son.list
+echo "deb http://http.re4son-kernel.com/re4son kali-pi-next main" > /etc/apt/sources.list.d/re4son.list
 wget -qO /etc/apt/trusted.gpg.d/kali_pi-archive-keyring.gpg https://re4son-kernel.com/keys/http/kali_pi-archive-keyring.gpg
 eatmydata apt-get update
 eatmydata apt-get install -y \$aptops kalipi-kernel kalipi-bootloader kalipi-re4son-firmware kalipi-kernel-headers
